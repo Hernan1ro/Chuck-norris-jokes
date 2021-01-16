61ed07660da1d33e56da2905911eb449779eef00
Hernán Mercado <hernandmf@gmail.com>
2021-01-16 03:46:13 -0500

    add and delete users

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+import * as CRUD from "./js/crud-provider.js";
+
+// CRUD.getUsuario(1).then(console.log);
+// CRUD.crearUsuario({
+//   name: "Fernando",
+//   job: "Carpintero",
+// }).then(console.log);
+
+// CRUD.actualizarUsuario(1, {
+//   name: "melisa",
+//   job: "Developer",
+// }).then(console.log);
+
+CRUD.borrarUsuario(1).then(console.log);
 // import { obtenerChiste } from "./js/http-provider";
 // obtenerChiste().then(console.log);
 
-import { init } from "./js/usuarios-page";
-// import { obtenerUsuarios } from "./js/http-provider";
+// import { init } from "./js/usuarios-page";
+// // import { obtenerUsuarios } from "./js/http-provider";
 
-init();
+// init();
 // obtenerUsuarios().then(console.log);
